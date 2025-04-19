db903a649cea6af1a7ac0c36664ce289ffbe5c69
ledeunf <JeanFrancois@gmail.com>
2025-04-19 15:56:02 +0200

    popup apropos et effet menu

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+//gestion du cheesburger (celui que je prefere !)
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Ajoute ou retire la classe 'active'
+    navLinks.classList.toggle('active'); 
+});
+
+//gestion du popup a propos
+const aboutLink = document.querySelector('.nav-links li:nth-child(2) a'); 
+const popup = document.getElementById('aboutPopup');  
+const closeBtn = document.querySelector('.close-btn'); 
+
+aboutLink.addEventListener('click', function(event) {
+    event.preventDefault();  
+    popup.style.display = 'block'; 
+});
+
+
+closeBtn.addEventListener('click', function() {
+    popup.style.display = 'none'; 
+});
+
+
+window.addEventListener('click', function(event) {
+    if (event.target === popup) {
+        popup.style.display = 'none'; 
+    }
 });
