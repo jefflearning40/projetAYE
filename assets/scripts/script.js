17ac5438b1c34ec748b0e240a68648b0e1167712
ledeunf <JeanFrancois@gmail.com>
2025-04-21 13:04:06 +0200

    creation page llibray et carousel

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,11 +6,11 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active'); 
 });
 
-//gestion du popup a propos
+//gestion du popcorn a propos
 document.addEventListener('DOMContentLoaded', () => {
     const aboutLink = document.querySelector('.nav-links li:nth-child(2) a'); 
     const popup = document.getElementById('aboutPopup');  
-    const closeBtn = document.querySelector('#aboutPopup .close-btn'); // Sélecteur modifié
+    const closeBtn = document.querySelector('#aboutPopup .close-btn'); 
 
     aboutLink.addEventListener('click', function(event) {
         event.preventDefault();  
