516c9e07d0b8c654baadcac09ade2bf9ab91f44d
ledeunf <jfld791000@gmail.com>
2025-05-13 12:55:27 +0200

    pack json gestion erreurs installé

--- dist/js/theme.js
+++ dist/js/theme.js
@@ -23,21 +23,38 @@ eval("\n\n//# sourceURL=webpack://Afec_starter_kit/./assets/scripts/base.js?");
 /*!*********************************!*\
   !*** ./assets/styles/base.scss ***!
   \*********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nThere's already a module with namespace \\\"reset\\\".\\n\\u001b[34m    ╷\\u001b[0m\\n\\u001b[34m1   │\\u001b[0m @use 'elements/reset';\\r\\n\\u001b[34m    │\\u001b[0m \\u001b[34m━━━━━━━━━━━━━━━━━━━━━\\u001b[0m\\u001b[34m original @use\\u001b[0m\\n\\u001b[34m... │\\u001b[0m\\n\\u001b[34m20  │\\u001b[0m \\u001b[31m@use 'elements/reset'\\u001b[0m;\\r\\n\\u001b[34m    │\\u001b[0m \\u001b[31m^^^^^^^^^^^^^^^^^^^^^\\u001b[0m\\u001b[31m new @use\\u001b[0m\\n\\u001b[34m    ╵\\u001b[0m\\n  assets\\\\styles\\\\base.scss 20:1  root stylesheet\\n    at tryRunOrWebpackError (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:86:9)\\n    at __webpack_require_module__ (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5301:12)\\n    at __webpack_require__ (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5258:18)\\n    at C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5330:20\\n    at symbolIterator (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:18:14)\\n    at C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5236:43\\n    at symbolIterator (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\neo-async\\\\async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nThere's already a module with namespace \\\"reset\\\".\\n\\u001b[34m    ╷\\u001b[0m\\n\\u001b[34m1   │\\u001b[0m @use 'elements/reset';\\r\\n\\u001b[34m    │\\u001b[0m \\u001b[34m━━━━━━━━━━━━━━━━━━━━━\\u001b[0m\\u001b[34m original @use\\u001b[0m\\n\\u001b[34m... │\\u001b[0m\\n\\u001b[34m20  │\\u001b[0m \\u001b[31m@use 'elements/reset'\\u001b[0m;\\r\\n\\u001b[34m    │\\u001b[0m \\u001b[31m^^^^^^^^^^^^^^^^^^^^^\\u001b[0m\\u001b[31m new @use\\u001b[0m\\n\\u001b[34m    ╵\\u001b[0m\\n  assets\\\\styles\\\\base.scss 20:1  root stylesheet\\n    at Object.<anonymous> (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[1].use[1]!C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[1].use[2]!C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[1].use[3]!C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\assets\\\\styles\\\\base.scss:1:7)\\n    at C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\javascript\\\\JavascriptModulesPlugin.js:494:10\\n    at Hook.eval [as call] (eval at create (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:14:14)\\n    at C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5303:39\\n    at tryRunOrWebpackError (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:81:7)\\n    at __webpack_require_module__ (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5301:12)\\n    at __webpack_require__ (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5258:18)\\n    at C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5330:20\\n    at symbolIterator (C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n\\nGenerated code for C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[1].use[1]!C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[1].use[2]!C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[1].use[3]!C:\\\\Users\\\\Stagiaire\\\\projetAYE\\\\assets\\\\styles\\\\base.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nThere's already a module with namespace \\\\\\\"reset\\\\\\\".\\\\n\\\\u001b[34m    ╷\\\\u001b[0m\\\\n\\\\u001b[34m1   │\\\\u001b[0m @use 'elements/reset';\\\\r\\\\n\\\\u001b[34m    │\\\\u001b[0m \\\\u001b[34m━━━━━━━━━━━━━━━━━━━━━\\\\u001b[0m\\\\u001b[34m original @use\\\\u001b[0m\\\\n\\\\u001b[34m... │\\\\u001b[0m\\\\n\\\\u001b[34m20  │\\\\u001b[0m \\\\u001b[31m@use 'elements/reset'\\\\u001b[0m;\\\\r\\\\n\\\\u001b[34m    │\\\\u001b[0m \\\\u001b[31m^^^^^^^^^^^^^^^^^^^^^\\\\u001b[0m\\\\u001b[31m new @use\\\\u001b[0m\\\\n\\\\u001b[34m    ╵\\\\u001b[0m\\\\n  assets\\\\\\\\styles\\\\\\\\base.scss 20:1  root stylesheet\\\");\");\n\n//# sourceURL=webpack://Afec_starter_kit/./assets/styles/base.scss?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://Afec_starter_kit/./assets/styles/base.scss?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	__webpack_modules__["./assets/scripts/base.js"]();
+/******/ 	__webpack_modules__["./assets/scripts/base.js"](0, {}, __webpack_require__);
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./assets/styles/base.scss"]();
+/******/ 	__webpack_modules__["./assets/styles/base.scss"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
+// Active les traces de dépréciation dans le terminal
+process.traceDeprecation = true;
+
 const webpack = require('webpack');
 const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssoWebpackPlugin = require('csso-webpack-plugin').default;
 const LicensePlugin = require('webpack-license-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin'); // ajouté
 
 const isProduction = process.env.NODE_ENV === 'production';
+const isWatch = process.argv.includes('--watch');
 
 module.exports = {
   mode: isProduction ? 'production' : 'development',
-
-  stats: 'errors-warnings', // ✅ réduit les messages inutiles
-  infrastructureLogging: {
-    level: 'error', // ✅ évite les logs système trop bavards
-  },
 
   entry: {
     theme: ['./assets/scripts/base.js', './assets/styles/base.scss'],
@@ -29,40 +28,67 @@ module.exports = {
     preferRelative: true,
   },
 
+  stats: 'errors-warnings',
+
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'esbuild-loader',
-        options: {
-          minify: isProduction,
-          target: 'es2015',
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            cacheDirectory: true,
+            compact: false,
+          },
         },
       },
+
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
-            options: { sourceMap: !isProduction },
+            options: {
+              sourceMap: !isProduction,
+            },
           },
           {
             loader: 'postcss-loader',
-            options: { sourceMap: !isProduction },
+            options: {
+              sourceMap: !isProduction,
+            },
           },
           {
             loader: 'sass-loader',
             options: {
               sourceMap: !isProduction,
-              implementation: require('sass'),
-              sassOptions: {
-                includePaths: [path.resolve(__dirname, 'assets/styles')], // ✅ pour résoudre les @use
-              },
             },
           },
         ],
       },
+
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: !isProduction,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: !isProduction,
+            },
+          },
+        ],
+      },
+
       {
         test: /\.(png|woff2?|eot|otf|ttf|svg|jpe?g|gif)(\?[a-z0-9=\.]+)?$/,
         type: 'asset/resource',
@@ -70,30 +96,23 @@ module.exports = {
           filename: 'dist/css/[hash][ext]',
         },
       },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { sourceMap: !isProduction },
-          },
-          {
-            loader: 'postcss-loader',
-            options: { sourceMap: !isProduction },
-          },
-        ],
-      },
     ],
   },
 
   plugins: [
-    new FriendlyErrorsWebpackPlugin(), // ✅ messages d’erreurs propres
     new MiniCssExtractPlugin({
       filename: path.join('..', 'css', '[name].css'),
     }),
+
+    // ✅ Ajout d’ESLintPlugin pour analyse JS
+    new ESLintPlugin({
+      extensions: ['js'],
+      emitWarning: true,
+      failOnError: false,
+    }),
+
     ...(isProduction ? [new CssoWebpackPlugin({ forceMediaMerge: true })] : []),
+
     new LicensePlugin({
       outputFilename: 'thirdPartyNotice.json',
       licenseOverrides: {
@@ -111,7 +130,9 @@ module.exports = {
             parallel: true,
             extractComments: false,
             terserOptions: {
-              compress: { drop_console: true },
+              compress: {
+                drop_console: true,
+              },
             },
           }),
         ],
@@ -119,4 +140,8 @@ module.exports = {
     : {
         minimize: false,
       },
+
+  infrastructureLogging: {
+    level: 'error',
+  },
 };
